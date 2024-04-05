35be8bd0717f96a5bd2bdfb51622bbec0fafb5a7
Stephane Torres <stephane.torres@madetech.com>
2024-04-05 11:00:41 +0100

    managed to get links to work with templating, hard coded though in base template

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 
 app = Flask(__name__)
 
+""" @app.context_processor
+def inject_navigation():
+    return dict(navigation = g.navigation)
 
 
+navigation = [{"name" : "home", "path" : 'hello'},
+              {"name" : 'gallery', "path" : 'gallery'},
+              { "name" : "info", "path" : "./templates/info.html"}] """
 
 
 @app.route("/")
@@ -11,12 +17,11 @@ def hello():
     return render_template('index.html')
 
 
-@app.route("/gallery")
+@app.route("/gallery/")
 def gallery():
     return render_template('gallery.html')
 
-navigation = [{"name" : "home", "path" : hello()},{"name" : 'gallery', "path" : gallery()}, { "name" : "info", "path" : "templates/info.html"}]
 
-@app.context_processor
-def inject_navigation():
-    return dict(navigation = navigation)
+
+
+
