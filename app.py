963064d835980f0a95cd813e68904c2fa964fdbb
Stephane Torres <stephane.torres@madetech.com>
2024-04-05 09:43:49 +0100

    attempting template pathing using an context injected dictionary

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-navigation = [{"name" : "home", "path" : "templates/index.html"},{"name" : 'gallery', "path" : "templates/gallery.html"}, { "name" : "info", "path" : "templates/info.html"}]
+
+
+
 
 @app.route("/")
 def hello():
@@ -12,3 +14,9 @@ def hello():
 @app.route("/gallery")
 def gallery():
     return render_template('gallery.html')
+
+navigation = [{"name" : "home", "path" : hello()},{"name" : 'gallery', "path" : gallery()}, { "name" : "info", "path" : "templates/info.html"}]
+
+@app.context_processor
+def inject_navigation():
+    return dict(navigation = navigation)
